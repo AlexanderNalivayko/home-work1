2afe3c78d8fb9ef5d9337a720b4a87026d12e07d
Alexander <nlvk48@gmail.com>
2019-03-21 12:27:38 +0200

    Homework1 (tasks 1 - 5)

---
+++ src/main/java/com/epam/training/javajunior/task1/BitHandlerUtils.java
@@ -0,0 +1,122 @@
+package com.epam.training.javajunior.task1;
+
+public class BitHandlerUtils {
+
+    private BitHandlerUtils() {
+    }
+
+    private static final int LONG_BITS = 64;
+    private static final int INT_BITS = 32;
+
+    public static int binaryToDecimal(String binaryString) {
+        if (binaryString == null || binaryString.isEmpty()) {
+            throw new IllegalArgumentException("binaryString can not be null or empty");
+        }
+        int length = binaryString.length();
+        if (length > INT_BITS) {
+            throw new NumberFormatException("binaryString length: "
+                    + length + " greater than " + INT_BITS);
+        }
+        for (int i = 0; i < length; i++) {
+            if (binaryString.charAt(i) != '0' && binaryString.charAt(i) != '1') {
+                throw new NumberFormatException("binaryString can not contain char: " + binaryString.charAt(i));
+            }
+        }
+        int decimal = 0;
+        boolean negative = (length == INT_BITS && binaryString.charAt(0) == '1');
+        for (int i = 0; i < length; i++) {
+            int bit;
+            if (negative) {
+                bit = Character.getNumericValue(binaryString.charAt((length - 1) - i) == '0' ? '1' : '0');
+            } else {
+                bit = Character.getNumericValue(binaryString.charAt((length - 1) - i));
+            }
+            decimal += bit * Math.pow(2, i);
+        }
+        return negative ? -(decimal + 1) : decimal;
+    }
+
+    public static int changeSign(int variable) {
+        return (~variable) + 1;
+    }
+
+    public static int countUsedBits(char value) {
+        return countPositiveValueBits(value);
+    }
+
+    public static int countUsedBits(short value) {
+        int bits = 0;
+        if (value < 0) {
+            value ^= Short.MIN_VALUE;
+            bits++;
+        }
+        return bits + countPositiveValueBits(value);
+    }
+
+    public static int countUsedBits(int value) {
+        int bits = 0;
+        if (value < 0) {
+            value ^= Integer.MIN_VALUE;
+            bits++;
+        }
+        return bits + countPositiveValueBits(value);
+    }
+
+    public static int countUsedBits(long value) {
+        int bits = 0;
+        if (value < 0) {
+            value ^= Long.MIN_VALUE;
+            bits++;
+        }
+        return bits + countPositiveValueBits(value);
+    }
+
+    private static int countPositiveValueBits(long num) {
+        if (num == 0) {
+            return 0;
+        }
+        for (int i = 1; i <= LONG_BITS; i++) {
+            if ((num >>= 1) == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int greaterCommonDivisor(int firstNum, int secondNum) {
+        while (firstNum != secondNum) {
+            if (firstNum > secondNum) {
+                firstNum = subtract(firstNum, secondNum);
+            } else {
+                secondNum = subtract(secondNum, firstNum);
+            }
+        }
+        return firstNum;
+    }
+
+    private static int subtract(int x, int y) {
+        while (y != 0) {
+            int borrow = (~x) & y;
+            x = x ^ y;
+            y = borrow << 1;
+        }
+        return x;
+    }
+
+    //bitIndex - starting from 0 to 31
+    //bitSetTo true - 1, false - 0
+    public static int setBit(int variable, int pos, boolean bitSetTo) {
+        if (pos >= INT_BITS) {
+            throw new IllegalArgumentException("bitIndex must bee less than " + INT_BITS);
+        }
+        int mask = 1;
+        for (int i = 0; i < pos; i++) {
+            mask <<= 1;
+        }
+        if (bitSetTo) {
+            return variable | mask;
+        } else {
+            return variable & ~mask;
+        }
+    }
+}
---
+++ src/main/java/com/epam/training/javajunior/task1/Runner.java
@@ -0,0 +1,60 @@
+package com.epam.training.javajunior.task1;
+
+import java.util.Scanner;
+
+import static com.epam.training.javajunior.task1.BitHandlerUtils.*;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        //1
+        String binaryString = getBinaryStringFromConsole();
+        print("binary number", binaryString, "int", binaryToDecimal(binaryString));
+
+        //2
+        int positiveInt = 48;
+        int negativeInt = -123;
+
+        print("number", positiveInt, "changed sign number", changeSign(positiveInt));
+        print("number", negativeInt, "changed sign number", changeSign(negativeInt));
+
+        //3
+        char charNumber = 11;
+        short positiveShort = 43;
+        short negativeShort = -31000;
+        long positiveLong = 85489498498L;
+        long negativeLong = -984869486468L;
+
+        print("number", (short) charNumber, "bits needed to store value", countUsedBits(charNumber));
+        print("number", positiveShort, "bits needed to store value", countUsedBits(positiveShort));
+        print("number", negativeShort, "bits needed to store value", countUsedBits(negativeShort));
+        print("number", positiveInt, "bits needed to store value", countUsedBits(positiveInt));
+        print("number", negativeInt, "bits needed to store value", countUsedBits(negativeInt));
+        print("number", positiveLong, "bits needed to store value", countUsedBits(positiveLong));
+        print("number", negativeLong, "bits needed to store value", countUsedBits(negativeLong));
+
+        //4
+        int firstNum = 21;
+        int secondNum = 27;
+        int thirdNum = 34;
+        int fourthNum = 17;
+
+        print("numbers", firstNum + ", " + secondNum, "gcd", greaterCommonDivisor(firstNum, secondNum));
+        print("numbers", thirdNum + ", " + fourthNum, "gcd", greaterCommonDivisor(thirdNum, fourthNum));
+
+        //5
+        print("before bit set", 1, "after bit set", setBit(1, 3, true));
+        print("before bit set", -1, "after bit set", setBit(-1, 31, false));
+    }
+
+    public static <T> void print(T firstNumName, T firstNum, T secondNumName, T SecondNum) {
+        System.out.printf("%s: %s | %s: %s%n", firstNumName, firstNum, secondNumName, SecondNum);
+    }
+
+    public static String getBinaryStringFromConsole() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Please enter bits, and press 'Enter':");
+            return scanner.nextLine();
+        }
+    }
+}
